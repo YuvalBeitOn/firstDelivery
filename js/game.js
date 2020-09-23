88def0be02eec907341b60e71e20f3e150f43fbb
YuvalBeitOn <71779473+YuvalBeitOn@users.noreply.github.com>
2020-09-23 20:23:03 +0300

    first-upload

--- js/game.js
+++ js/game.js
@@ -15,27 +15,24 @@ var gLevel = {
     MINES: 2
 };
 
-var gBoard = buildBoard();
-
+var gBoard;
 var gGame = {
-    isOn: false,
+    isOn: true,
     shownCount: 0,
     markedCount: 0,
     secsPassed: 0
 };
 
-
 function initGame() {
-    setDifficulty();
-    gGame.isOn = true;
-    gGame.shownCount = 0;
-    gGame.markedCount = 0;
-    gGame.secsPassed = 0;
+    gBoard = buildBoard();
+    placeMines(gBoard);
+    updateCountMines(gBoard);
     renderBoard(gBoard);
 }
 
 function buildBoard() {
     // Builds the board
+    // setDifficulty();
     var board = [];
     for (var i = 0; i < gLevel.SIZE; i++) {
         board[i] = [];
@@ -48,20 +45,8 @@ function buildBoard() {
             };
         };
     }
-    board[3][2] = {
-        minesAroundCount: 0,
-        isShown: false,
-        isMine: true,
-        isMarked: false
-    };
-    board[1][3] = {
-        minesAroundCount: 0,
-        isShown: false,
-        isMine: true,
-        isMarked: false
-    };
     console.table(board);
-    updateCountMines(board);
+    // updateCountMines(board);
     return board;
 
     // Set mines at random locations
@@ -69,25 +54,58 @@ function buildBoard() {
     // Return the created board
 }
 
-// function placeMines();
+function placeMines(board) {
+    var emptyPoses = [];
+    for (var i = 0; i < board.length; i++) {
+        for (var j = 0; j < board[i].length; j++) {
+            var cell = board[i][j];
+            var pos = { i: i, j: j };
+            emptyPoses.push(pos);
+        }
+    }
+    for (var i = 0; i < gLevel.MINES; i++) {
+        var randPos = emptyPoses[getRandomIntInclusive(0, emptyPoses.length)];
+        board[randPos.i][randPos.j].isMine = true;
+    }
+}
+
+// function expandShown(pos) { // כאשר המשתמש לוחץ על תא עם המספר 0 שאין מסביבו מוקשים
+//     if (gBoard[pos.i][pos.j].minesAroundCount === 0) {
+//         for (var i = pos.i - 1; i <= pos.i + 1; i++) {
+//             if (i < 0 || i >= gBoard.length) continue;
+//             for (var j = pos.j - 1; j <= pos.j + 1; j++) {
+//                 if (j < 0 || j >= gBoard[i].length) continue;
+//                 if (pos.i === i && pos.j === j) continue;
+//                 var cell = gBoard[i][j];
+//                 if (cell === MINE) return;
+//                 else {
+//                     cell.isShown = true;
+//                 }
+//             }
+//         }
+//     }
+// }
 
 
 function setDifficulty(elBtn) {
-
     switch (elBtn.name) {
         case 'Begginer':
             gLevel.SIZE = 4;
+            gLevel.MINES = 2;
+            initGame();
             break;
         case 'Medium':
-            gLevel.SIZE = 4;
+            gLevel.SIZE = 8;
+            gLevel.MINES = 12;
+            initGame();
             break;
         case 'Expert':
-            gLevel.SIZE = 4;
+            gLevel.SIZE = 12;
+            gLevel.MINES = 30;
+            initGame();
             break;
     }
-    init();
 }
-
 
 function updateCountMines(board) {
     for (var i = 0; i < board.length; i++) {
@@ -106,10 +124,10 @@ function renderBoard(board) {
         strHTML += '<tr>\n';
         for (var j = 0; j < board[0].length; j++) {
             var currCell = board[i][j];
-
+            var pos = { i: gBoard[i], j: gBoard[j] };
             var cellClass = getClassName({ i: i, j: j })
 
-            strHTML += '\t<td class="cell ' + cellClass + ' " oncontextmenu = "cellMarked(event,' + i + ',' + j + ')" onclick="cellClicked(' + i + ',' + j + ')" >\n';
+            strHTML += '\t<td class="cell ' + cellClass + ' " oncontextmenu = "cellMarked(event,' + i + ',' + j + ')" onclick="cellClicked(' + i + ',' + j + ')">\n';
 
             if (!currCell.isShown && currCell.isMarked) {
                 strHTML += FLAG;
@@ -161,7 +179,7 @@ function setMinesNegsCount(board, pos) { // pos - object {i: i, j: j}
 
 // console.log(setMinesNegsCount(gBoard, { i: 2, j: 3 }));
 
-function cellClicked(i, j) { // איך לעשות עם rendercell
+function cellClicked(i, j) {
     var currCell = gBoard[i][j];
     if (currCell.isShown) return;
     currCell.isShown = true;
